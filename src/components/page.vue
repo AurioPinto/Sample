11182b04b5b8f87f2378cab803f2aa8f583c23b1
AurioPinto <auriopinto@outlook.com>
2020-02-28 12:44:13 +0800

    changed indexes

--- src/components/page.vue
+++ src/components/page.vue
@@ -1,54 +1,55 @@
 <template>
+  <div>
     <div>
-      <div>
-        <h1>{{greet('Morning')}}</h1>
-        <p>Name: {{name}}</p>
-        <p>job: {{job}}</p>
-        <h1>Data Binding</h1>
-        <a v-bind:href="website">Aurio</a>
-      </div>
-      <div>
-        <h1>Events</h1>
-        <button @click.once="add(1)">Add a Year</button>
-        <button @click="subtract(1)">Subtract Year</button>
-        <p>My age is {{age}}</p>
-      </div>
-      <div id="canvas" @mousemove="updateXY">{{x}}, {{y}}</div>
-      <!-- computed properties -->
-      <div>
-        <h1>Computed properties</h1>
-        <button @click="a++">Add to A</button>
-        <button @click="b++">Add to B</button>
-        <p>A - {{a}}</p>
-        <p>B - {{b}}</p>
-        <p>Age + a = {{addToA}}</p>
-        <p>Age + b = {{addToB}}</p>
-      </div>
-      <h2>Machine</h2>
-      <button @click="nearby = !nearby">Toggle nearby</button>
-      <button @click="available =!available">Toggle available</button>
-      <div v-bind:class="compClasses">
-        <span>Ryu</span>
-      </div>
-      <!-- Conditional  -->
-      <h1>Conditional</h1>
-      <button @click="error = !error">Toggle Error</button>
-      <button @click="sucess = !sucess">Toggle Sucess</button>
-      <!-- <p v-if="error">There has been an error</p> -->
-      <!-- <p v-else-if="sucess">This message has sucess, congrats!</p> -->
-      <p v-show="error">There has been a error</p>
-      <p v-show="sucess">whoooo! This sucess</p>
-      <div>
-        <h1>Looping through list</h1>
-        <ul>
-          <li class="list" v-for="character in characters" :key="character">{{character}}</li>
-        </ul>
-        <ul>
-          <li
-            v-for="(apdevices,index) in apdevices"
-            :key="apdevices"
-          >{{index}} . {{apdevices.name}} - {{apdevices.age}}</li>
-        </ul>
+      <h1>{{ greet("Morning") }}</h1>
+      <p>Name: {{ name }}</p>
+      <p>job: {{ job }}</p>
+      <h1>Data Binding</h1>
+      <a v-bind:href="website">Aurio</a>
+    </div>
+    <div>
+      <h1>Events</h1>
+      <button @click.once="add(1)">Add a Year</button>
+      <button @click="subtract(1)">Subtract Year</button>
+      <p>My age is {{ age }}</p>
+    </div>
+    <div id="canvas" @mousemove="updateXY">{{ x }}, {{ y }}</div>
+    <!-- computed properties -->
+    <div>
+      <h1>Computed properties</h1>
+      <button @click="a++">Add to A</button>
+      <button @click="b++">Add to B</button>
+      <p>A - {{ a }}</p>
+      <p>B - {{ b }}</p>
+      <p>Age + a = {{ addToA }}</p>
+      <p>Age + b = {{ addToB }}</p>
+    </div>
+    <h2>Machine</h2>
+    <button @click="nearby = !nearby">Toggle nearby</button>
+    <button @click="available = !available">Toggle available</button>
+    <div v-bind:class="compClasses">
+      <span>Ryu</span>
+    </div>
+    <!-- Conditional  -->
+    <h1>Conditional</h1>
+    <button @click="error = !error">Toggle Error</button>
+    <button @click="sucess = !sucess">Toggle Sucess</button>
+    <!-- <p v-if="error">There has been an error</p> -->
+    <!-- <p v-else-if="sucess">This message has sucess, congrats!</p> -->
+    <p v-show="error">There has been a error</p>
+    <p v-show="sucess">whoooo! This sucess</p>
+    <div>
+      <h1>Looping through list</h1>
+      <ul>
+        <li class="list" v-for="character in characters" :key="character">
+          {{ character }}
+        </li>
+      </ul>
+      <ul>
+        <li v-for="(apdevices, index) in apdevices" :key="apdevices">
+          {{ index }} . {{ apdevices.name }} - {{ apdevices.age }}
+        </li>
+      </ul>
       <!-- </div> -->
       <!-- <div v-for="(apdevices,index )in apdevices" :key="apdevices">
       <h3>{{index}} . {{apdevices.name}}</h3>
@@ -62,6 +63,7 @@
       </div>-->
       <!-- <screen></screen> -->
     </div>
+  </div>
 </template>
 
 <script>
@@ -77,7 +79,7 @@ export default {
       nearby: false,
       error: false,
       sucess: false,
-      output: "PMy favorite language",
+      output: "My favorite language",
       characters: ["Aurio", "Machine", "tech", "computer-vision", "beyond"],
       apdevices: [
         { name: "Aurio", age: 25 },
@@ -161,7 +163,4 @@ span {
   content: nearby;
   margin-left: 10px;
 }
-// .list {
-//   text-align: center;
-// }
 </style>
